f4f288f26235797ed0ccd0e9eee51cb8a715a284
Paul Goulart <paul.goulart@eng.ox.ac.uk>
2018-07-23 16:40:36 +0100

    loop restructure

--- src/qdldl.c
+++ src/qdldl.c
@@ -68,12 +68,15 @@ QDLDL_int QDLDL_etree(const QDLDL_int  n,
           etree[i] = j;
         }
         Lnz[i]++;         //nonzeros in this column
-        sumLnz += 1;      // Increase the total number of nonzeros
         work[i] = j;
         i = etree[i];
       }
     }
   }
+
+  //compute the total nonzeros in L.  This much
+  //space is required to store Li and Lx
+  for(i = 0; i < n; i++){sumLnz += Lnz[i];}
 
   return sumLnz;
 }
